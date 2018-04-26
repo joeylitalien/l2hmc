4bc6821632576444c468531ed2ea1ed731ddf58a
Joey Litalien <joey.litalien@gmail.com>
2018-04-26 03:40:51 -0400

    Fixed indenting in distributions and added more utility functions for the notebook

--- l2hmc/utils/distributions.py
+++ l2hmc/utils/distributions.py
@@ -36,15 +36,15 @@ def random_tilted_gaussian(dim, log_min=-2., log_max=2.):
   R = ortho_group.rvs(dim)
   sigma = np.diag(np.exp(np.log(10.) * np.random.uniform(log_min, log_max, size=(dim,)))) + 1e-6 * np.eye(dim)
   S = R.T.dot(sigma).dot(R)
-  return Gaussian(mu, S)
-    
+  return mu, S
+
 class Gaussian(object):
   def __init__(self, mu, sigma):
     self.mu = mu
     self.sigma = sigma
-    
+
     print(np.linalg.det(self.sigma), self.sigma.dtype)
-    
+
     self.i_sigma = np.linalg.inv(np.copy(sigma))
 
   def get_energy_function(self):
@@ -74,7 +74,7 @@ class TiltedGaussian(Gaussian):
     S = self.R.T.dot(self.diag).dot(self.R)
     self.dim = dim
     Gaussian.__init__(self, np.zeros((dim,)), S)
-    
+
   def get_samples(self, n):
     X = np.random.randn(200, self.dim)
     X = X.dot(np.sqrt(self.diag))
@@ -139,7 +139,7 @@ class GMM(object):
 
     samples = []
 
-    for k, v in counter_samples.iteritems():
+    for k, v in counter_samples.items():
       samples.append(np.random.multivariate_normal(self.mus[k], self.sigmas[k], size=(v,)))
 
     samples = np.concatenate(samples, axis=0)
--- l2hmc/utils/notebook_utils.py
+++ l2hmc/utils/notebook_utils.py
@@ -3,6 +3,8 @@ import numpy as np
 from dynamics import Dynamics
 from sampler import propose
 import matplotlib.pyplot as plt
+from scipy.stats import multivariate_normal
+
 
 def plot_grid(S, width=8):
     sheet_width = width
@@ -12,7 +14,7 @@ def plot_grid(S, width=8):
         plt.imshow(S[i], cmap='gray')
         plt.grid('off')
         plt.axis('off')
-        
+
 def plot_line(S):
     sheet_width = S.shape[0]
     plt.figure(figsize=(16, 3))
@@ -21,19 +23,65 @@ def plot_line(S):
         plt.imshow(S[i], cmap='gray')
         plt.grid('off')
         plt.axis('off')
-        
+
 def get_hmc_samples(x_dim, eps, energy_function, sess, T=10, steps=200, samples=None):
     hmc_dynamics = Dynamics(x_dim, energy_function, T=T, eps=eps, hmc=True)
     hmc_x = tf.placeholder(tf.float32, shape=(None, x_dim))
     Lx, _, px, hmc_MH = propose(hmc_x, hmc_dynamics, do_mh_step=True)
-    
+
     if samples is None:
         samples = gaussian.get_samples(n=200)
-        
+
     final_samples = []
-    
+
     for t in range(steps):
         final_samples.append(np.copy(samples))
         Lx_, px_, samples = sess.run([Lx, px, hmc_MH[0]], {hmc_x: samples})
-        
+
     return np.array(final_samples)
+
+def plot_gaussian_contours(mus, covs, colors=['blue', 'red'], spacing=5,
+        x_lims=[-4,4], y_lims=[-3,3], res=100):
+
+    X = np.linspace(x_lims[0], x_lims[1], res)
+    Y = np.linspace(y_lims[0], y_lims[1], res)
+    X, Y = np.meshgrid(X, Y)
+    pos = np.empty(X.shape + (2,))
+    pos[:, :, 0] = X
+    pos[:, :, 1] = Y
+
+    for i in range(len(mus)):
+        mu = mus[i]
+        cov = covs[i]
+        F = multivariate_normal(mu, cov)
+        Z = F.pdf(pos)
+        plt.contour(X, Y, Z, spacing, colors=colors[0])
+
+    return plt
+
+    #gaussian_1 = plt.contour(X, Y, Z_1, 5, colors=main)
+    #gaussian_2 = plt.contour(X, Y, Z_2, 5, colors=main)
+    #plt.plot(HMC_samples_1[:50, 1, 0], HMC_samples_1[:50, 1, 1], color=secondary, marker='o', alpha=0.75)
+    #plt.axis('equal')
+    #plt.show()
+
+"""
+from scipy.stats import multivariate_normal
+N = 100
+X = np.linspace(-3, 3, N)
+Y = np.linspace(-2, 2, N)
+X, Y = np.meshgrid(X, Y)
+mu_1 = np.array([-2, 0])
+mu_2 = np.array([2, 0])
+cov_1 = 0.1*np.eye(2)
+cov_2 = 0.1*np.eye(2)
+F_1 = multivariate_normal(mu_1, cov_1)
+F_2 = multivariate_normal(mu_2, cov_2)
+pos = np.empty(X.shape + (2,))
+pos[:, :, 0] = X
+pos[:, :, 1] = Y
+Z_1 = F_1.pdf(pos)
+Z_2 = F_2.pdf(pos)
+gaussian_1 = plt.contour(X, Y, Z_1, 5, colors=main)
+gaussian_2 = plt.contour(X, Y, Z_2, 5, colors=main)
+"""
